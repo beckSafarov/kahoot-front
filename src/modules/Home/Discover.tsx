7b87da4e10c95cb0ecc09b242673097b1020cf14
Begzod Safarov <begzod.safarov07@gmail.com>
2023-06-04 11:56:10 +0500

    fixed a type error

--- src/modules/Home/Discover.tsx
+++ src/modules/Home/Discover.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Dashboard from './components/Dashboard'
 
 const allKahoots = [
-  { title: 'Car Logos', image: '/images/car.webp', author: 'Begzod Safarov' },
-  { title: 'Cool Cars', image: '/images/car.webp', author: 'John Doe' },
+  { title: 'Car Logos', image: '/images/car.webp', author: 'Begzod Safarov', coverImage: '', },
+  { title: 'Cool Cars', image: '/images/car.webp', author: 'John Doe', coverImage: '' },
 ]
 
 const Discover = () => {
